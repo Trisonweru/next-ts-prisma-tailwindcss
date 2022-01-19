6f7dd9c69463a5648e2f643b354f3f8a1f45fab4
Trisonweru <isaactrisonwaweru@gmail.com>
2022-01-20 00:00:58 +0300

    third commit

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 /* eslint-disable @next/next/no-html-link-for-pages */
 /* eslint-disable @next/next/no-img-element */
 import { useRouter } from 'next/router';
